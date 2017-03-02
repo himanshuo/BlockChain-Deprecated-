c954345367f80357f33a07e11a074d4414b346d6
himanshu <himanshuo@gmail.com>
2017-03-02 05:09:36 -0800

    add tests

---
+++ src/test/java/com/opensource/app/BlockChainTest.java
@@ -0,0 +1,104 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BlockChainTest {
+
+  @Before
+  public void before(){
+
+  }
+
+  @After
+  public void after(){
+      Internet.reset();
+  }
+
+  @Test
+  public void testBlockChain(){
+    Client a = new Client(6);
+    Client b = new Client(5);
+    Client c = new Client(1);
+
+
+    assertEquals("A has invalid number of coins", 6, a.coins);
+    assertEquals("B has invalid number of coins", 5, b.coins);
+    assertEquals("C has invalid number of coins", 1, c.coins);
+
+    a.send(3,b);
+
+    a.send(2,b);
+
+    c.send(1,b);
+
+    b.send(3,a);
+
+    assertEquals("A has invalid number of coins post sending", 4, a.coins);
+    assertEquals("B has invalid number of coins post sending", 8, b.coins);
+    assertEquals("C has invalid number of coins post sending", 0, c.coins);
+
+
+    //each knows about the transaction history
+    assertEquals("A.ledger != B.ledger", a.ledger.toString(), b.ledger.toString());
+    assertEquals("A.ledger != C.ledger", a.ledger.toString(), c.ledger.toString());
+    assertEquals("B.ledger != C.ledger", b.ledger.toString(), c.ledger.toString());
+  }
+
+
+
+    @Test
+    public void testSendSingle(){
+        Client a = new Client(6);
+        Client b = new Client(5);
+
+        assertEquals(6, a.coins);
+        assertEquals(5, b.coins);
+
+        assertTrue(a.send(b, 3));
+
+        assertEquals(3, a.coins);
+        assertEquals(8, b.coins);
+
+        assertEquals(1, a.ledger.size());
+        assertEquals(a.ledger.get(0).hash, b.ledger.get(0).hash);
+        assertEquals(a.ledger.get(0).amount, b.ledger.get(0).amount);
+        assertEquals(3, a.ledger.get(0).amount);
+    }
+
+    @Test
+    public void testSendDouble(){
+        Client a = new Client(6);
+        Client b = new Client(5);
+
+        assertEquals(6, a.coins);
+        assertEquals(5, b.coins);
+
+        assertTrue(a.send(b, 3));
+
+        assertEquals(3, a.coins);
+        assertEquals(8, b.coins);
+
+        assertEquals(1, a.ledger.size());
+        assertEquals(a.ledger.get(0).hash, b.ledger.get(0).hash);
+        assertEquals(a.ledger.get(0).amount, b.ledger.get(0).amount);
+        assertEquals(3, a.ledger.get(0).amount);
+
+        assertTrue(b.send(a, 2));
+
+        assertEquals(5, a.coins);
+        assertEquals(6, b.coins);
+
+        assertEquals(2, a.ledger.size());
+
+        assertEquals(a.ledger.get(0).hash, b.ledger.get(0).hash);
+        assertEquals(a.ledger.get(0).amount, b.ledger.get(0).amount);
+        assertEquals(a.ledger.get(1).hash, b.ledger.get(1).hash);
+        assertEquals(a.ledger.get(1).amount, b.ledger.get(1).amount);
+
+        assertEquals(3, a.ledger.get(0).amount);
+        assertEquals(2, a.ledger.get(1).amount);
+    }
+
+}
---
+++ src/test/java/com/opensource/app/lib/HashTest.java
@@ -0,0 +1,32 @@
+package lib;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by himanshu on 12/10/16.
+ */
+public class HashTest {
+    @Test
+    public void hash() throws Exception {
+
+    }
+
+    @Test
+    public void toHexString() throws Exception {
+        String testCase = "Hello, world!0";
+        String expected = "1312af178c253f84028d480a6adc1e25e81caa44c749ec81976192e2ec934c64";
+        String actual = Hash.toHexString(Hash.hash(testCase));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void doubleHash() throws Exception {
+        String testCase = "Hello, world!0";
+        String expected = "c90b111659c9b88f5966e4966e03480721af9175f1636172935011fbcf3a9f57";
+        String actual = Hash.toHexString(Hash.doubleHash(testCase));
+        assertEquals(expected, actual);
+    }
+
+}
